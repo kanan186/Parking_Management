4ad461c5219d416655421a90ddf305ee0525c417
kanan186 <kananvadher@gmail.com>
2025-02-05 11:46:57 +0530

    Add project files.

---
+++ ParkingManagement/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ParkingManagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
